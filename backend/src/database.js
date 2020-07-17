7f0fd7d71bd28be8c4f2a5a429b9bab04c544e84
Micol <micolpa08@gmail.com>
2020-07-17 09:11:19 -0400

    Creating Project and Database Connection

---
+++ backend/src/database.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const URI = process.env.MONGODB_URI ? process.env.MONGODB_URI : 'mongodb://localhost/databasetest';
+mongoose.connect(URI, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
+
+const connection = mongoose.connection;
+
+//Once is connected the DB it'll show me this message
+connection.once('open', () => {
+  console.log("DB Connected");
+})
